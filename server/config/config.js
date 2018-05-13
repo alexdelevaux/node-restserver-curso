8f360570445203ca1aee96c7f73153c02622f4d5
Alejandro <acdelevaux@gmail.com>
2018-05-13 11:50:22 -0300

    correcciones del servidor

--- server/config/config.js
+++ server/config/config.js
@@ -26,7 +26,9 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 }
 else {
-    urlDB = 'mongodb://cafe-user:on7117!!MLAB!!@ds221990.mlab.com:21990/cafe';
+    urlDB = 'mongodb://usuario-cafe:mlab17@ds221990.mlab.com:21990/cafe';
 }
 
 process.env.URL_DB = urlDB;
+
+
